204d1fd37e9c623e829b0076fde1fc239fbd1189
haikal <muhamad.haekal@go-jek.com>
2017-05-14 00:48:29 +0700

    add categories to homepage

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -6,6 +6,7 @@ use App\Criteria\LatestCriteria;
 use App\Criteria\LimitCriteria;
 use App\Criteria\RandomCriteria;
 use App\Http\Controllers\Controller;
+use App\Repositories\CategoryRepository;
 use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 
@@ -17,16 +18,17 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(ProductRepository $productRepo)
+    public function index(ProductRepository $productRepo, CategoryRepository $catRepo)
     {
         $productRepo->pushCriteria(LatestCriteria::class);
 
         $products = $productRepo->paginate();
+        $categories = $catRepo->all();
         $productRandoms = $productRepo->pushCriteria(RandomCriteria::class)
                                     ->pushCriteria(new LimitCriteria(3))
                                     ->all();
 
-        return view('front.home', compact('products', 'productRandoms'));
+        return view('front.home', compact('products', 'productRandoms', 'categories'));
     }
 
 }
