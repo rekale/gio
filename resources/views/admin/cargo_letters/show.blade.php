c58c497dc068696aced7402c8272b2965e915fb0
haikal <muhamad.haekal@go-jek.com>
2017-05-27 07:17:06 +0700

    show products detail in cargo letter show

--- resources/views/admin/cargo_letters/show.blade.php
+++ resources/views/admin/cargo_letters/show.blade.php
@@ -11,6 +11,32 @@
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
                     @include('admin.cargo_letters.show_fields')
+                    <div class="clearfix"></div>
+                    <div class="content">
+                        <table class="table table-responsive" id="travelDocuments-table">
+                            <thead>
+                                <th>Product Name</th>
+                                <th>Quantity</th>
+                                <th>Note</th>
+                            </thead>
+                            <tbody id="product">
+
+                                @foreach($cargoLetter->products as $cargoProduct)
+                                    <tr>
+                                        <td>
+                                            {{$cargoProduct->name}}
+                                        </td>
+                                         <td>
+                                            {{ $cargoProduct->pivot->quantity }}
+                                        </td>
+                                        <td>
+                                            {{ $cargoProduct->pivot->note ?? '-' }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                     <a href="{!! route('admin.cargoLetters.index') !!}" class="btn btn-default">Back</a>
                 </div>
             </div>
